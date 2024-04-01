3ac3f8f32afed7f61b3910c81d55ae4ca9be69b5
Chirag-Punia <cpunia_be21@thapar.edu>
2024-04-01 16:27:55 +0530

    node modules deleted

--- app.js
+++ app.js
@@ -2,7 +2,19 @@ const express = require('express');
 
 const app = express();
 
+const expressLayouts = require("express-ejs-layouts");
+
 const PORT = process.env.PORT || 5000;
+
+//routes
+
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+
+app.use('/' , require("./routes/index"));
+
+app.use('/users' , require("./routes/users"));
+
 
 app.listen(PORT,() => {
     console.log(`App started at PORT ${PORT}`)
