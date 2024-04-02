5efb8d557ee490e341be3cae6fce7757e9c6b37d
Chirag-Punia <cpunia_be21@thapar.edu>
2024-04-02 22:14:51 +0530

    welcome.ejs bug fixed

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,10 @@ const express = require("express");
 const router = express.Router();
 const {ensureAuthenticated} = require("../config/auth");
 router.get("/", (req,res) => {
-    res.render("Welcome");
+    res.render("welcome");
 })
 router.get("/dashboard",ensureAuthenticated ,(req,res) => {
     res.render("dashboard");
 })
-
 
 module.exports = router;
